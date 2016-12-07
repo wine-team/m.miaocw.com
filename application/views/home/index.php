6d9a64cca87daf07e5afd27f2e3ca6319a4ea81b
604942578 <604942578@qq.com>
2016-12-08 07:56:25 +0800

    M

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,4 +1,247 @@
 <?php $this->load->view('layout/header');?>
+<div id="top">
+	<div class="header hfix" id="hometop">
+		<a class="logo" href="tel:4006600606"></a>
+		<a href="fenlei.html" class="h_t h_fl">分类</a>
+		<form action="search.php" class="t_se" onSubmit="return se(this)">
+			<input type="search" value="" class="t_sl" id="t_sl" name="keyword" placeholder="搜索商品" />
+			<input class="t_sr" type="submit" value="" />
+		</form>
+		<a href="chaxun.php" class="h_t h_wl">物流</a>
+	</div>
+	<div class="sebg hid" id="sebg">
+		<div class="lr10 lh35 ov">
+			<em class="left">热门搜索</em>
+			<a href="javascript:;" onClick="gx()" class="right">关闭</a>
+		</div>
+		<a href="search.php?keyword=仿真阳具" class="hw"><em class="red">仿真阳具</em></a>
+		<a href="search.php?keyword=延时" class="hw">男用延时</a>
+		<a href="search.php?keyword=飞机杯" class="hw">飞机杯</a>
+		<a href="search.php?keyword=双点刺激" class="hw">双点刺激</a>
+		<a href="search.php?keyword=跳蛋" class="hw">跳蛋</a>
+		<a href="search.php?keyword=倒模" class="hw">真人倒模</a>
+		<a href="search.php?keyword=增大" class="hw">增大器</a>
+	</div>
+</div>
+<div class="page load">
+	<div class="slider" id="slider">
+		<ul class="sul">
+			<li><a href="goods-5503.html?mps=focus"><img src="http://s.qw.cc/data/afficheimg/1451545231672443191.jpg" width="100%"/></a></li>
+			<li><a href="goods-5415.html?mps=focus"><img lazy="http://s.qw.cc/data/afficheimg/1460015835743002712.jpg" width="100%"/></a></li>
+			<li><a href="goods-5520.html?mps=focus"><img lazy="http://s.qw.cc/data/afficheimg/1456996975205359985.jpg" width="100%"/></a></li>
+		</ul>
+		<div class="btn alC" id="btn"></div>
+	</div>
+	<div class="bgw pt10">
+		<a href="man/" class="da"><img src="http://s.qw.cc/mobile/ui/b1.png" class="dam"/><p>男用</p></a>
+		<a href="woman/" class="da"><img src="http://s.qw.cc/mobile/ui/b2.png" class="dam"/><p>女用</p></a>
+		<a href="baojian/" class="da"><img src="http://s.qw.cc/mobile/ui/b3.png" class="dam"/><p>延时</p></a>
+		<a href="neiyi/" class="da"><img src="http://s.qw.cc/mobile/ui/b4.png" class="dam"/><p>情趣内衣</p></a>
+		<a href="woman/bizhen/" class="da"><img src="http://s.qw.cc/mobile/ui/b51.png" class="dam"/><p>仿真阳具</p></a>
+		<a href="man/feijibei/" class="da xx"><img src="http://s.qw.cc/mobile/ui/b6.png" class="dam"/><p>飞机杯</p></a>
+		<a href="xingai/" class="da xx"><img src="http://s.qw.cc/mobile/ui/b7.png" class="dam"/><p>SM情趣</p></a>
+		<a href="topic.php?topic_id=687" class="da"><img src="http://s.qw.cc/mobile/ui/b8.png" class="dam"/><p>土豪玩具</p></a>
+		<a href="https://itunes.apple.com/cn/app/qu-wang-qing-qu-shang-cheng/id934737326" class="da"><img src="http://s.qw.cc/mobile/ui/b9.png" class="dam"/><p>APP下载</p></a>
+		<a href="fenlei.html" class="da"><img src="http://s.qw.cc/mobile/ui/b10.png" class="dam"/><p>全部分类</p></a>
+		<div class="clear"></div>
+	</div>
+	<div class="bgw mt10">
+		<a href="loot.php" class="left lotm">
+			<div class="hoot">
+				<p class="alL">玩爆潮品喵星人天使之杖av按摩棒</p>
+				<img src="http://s.qw.cc/images/201410/thumb_img/5474_thumb_P220_1413196593328.jpg" class="limg">
+			    <p><b class="xj">¥149.00</b><del class="pl10">¥249.00</del></p>
+			 </div>
+			 <img src="http://s.qw.cc/mobile/css/home/thr1.png" width="100%">
+	    </a>
+		<div class="dhr left">
+			<a href="hot.html" class="lotm2 left"><img src="http://s.qw.cc/mobile/css/home/thr2.png" width="100%"></a>
+			<a href="new.html" class="lotm3 left"><img src="http://s.qw.cc/mobile/css/home/thr31.png" width="100%"></a>
+			<a href="need.php" class="lotm3 left"><img src="http://s.qw.cc/mobile/css/home/thr5.png" width="100%"></a>
+		</div>
+	</div>
+	<div id="lazy">
+		<div class="bgw mt10">
+			<h2 class="t">
+				<a href="woman/" class="brl">女性用品</a>
+				<a href="woman/" class="more right"></a>
+			</h2>
+			<div class="hpre ov" style="background-color:#bd3d5e">
+				<a href="goods-6017.html?mps=index" class="w64 left"><img src="http://s.qw.cc/data/afficheimg/1460015902548090829.jpg" width="100%"/></a>
+				<a href="goods-8806.html?mps=index" class="w36 left"><img src="http://s.qw.cc/data/afficheimg/1460015949105508640.jpg" width="100%"/></a>
+			</div>
+		</div>
+		<div class="m_z ov">
+			<a href="woman/gdian/" class="red fB">G点刺激</a>
+			<a href="woman/zhuanzhu/">转珠棒</a>
+			<a href="woman/bizhen/" class="red fB">仿真阳具</a>
+			<a href="woman/tiaodan/">跳蛋</a>
+			<a href="woman/aidejiqi/">性爱机器</a>
+			<a href="woman/xbcj/">双乳刺激</a>
+			<a href="woman/sdcj/" class="red fB">按摩棒</a>
+			<a href="woman/">更多></a>
+		</div>
 
 
+		<div class="bgw mt10">
+			<h2 class="t">
+				<a href="man/" class="brl">男性用品</a>
+				<a href="man/" class="more right"></a>
+			</h2>
+			<div class="hpre ov" style="background-color:#fadceb">
+				<a href="goods-7577.html?mps=index" class="w36 left"><img src="http://s.qw.cc/data/afficheimg/1445581303359909308.jpg" width="100%"/></a>
+				<a href="goods-9221.html?mps=index" class="w64 left"><img src="http://s.qw.cc/data/afficheimg/1462437311787080742.jpg" width="100%"/></a>
+			</div>
+		</div>
 
+		<div class="m_z ov">
+			<a href="man/feijibei/">自慰飞机杯</a>
+			<a href="man/daomo/" class="red fB">女优名器</a>
+			<a href="man/meitun/">性感美臀</a>
+			<a href="man/cqww/" class="red fB">充气娃娃</a>
+			<a href="man/yanshi/">延时训练</a>
+			<a href="man/nangen/" class="red fB">男根增大</a>
+			<a href="man/wanou/">实体玩偶</a>
+			<a href="man/">更多></a>
+		</div>
+
+		<div class="hmg" style="background-color:#0c0d0f;">
+			<a href="topic.php?topic_id=687">
+				<img src="http://s.qw.cc/data/afficheimg/1459243878728984124.jpg">
+			</a>
+		</div>
+
+		<div class="bgw mt10">
+			<h2 class="t">
+				<a href="baojian/" class="brl">延时助情</a>
+				<a href="baojian/" class="more right"></a>
+			</h2>
+			<div class="hpre ov" style="background-color:#fc588d">
+				<a href="goods-5415.html?mps=index" class="w64 left"><img src="http://s.qw.cc/data/afficheimg/1441012813280750922.jpg" width="100%"/></a>
+				<a href="goods-6007.html?mps=index" class="w36 left"><img src="http://s.qw.cc/data/afficheimg/1441012359509261528.jpg" width="100%"/></a>
+			</div>
+		</div>
+
+		<div class="m_z ov">
+			<a href="baojian/yanshi/" class="red fB">男用延时</a>
+			<a href="baojian/zhubo/">增大助勃</a>
+			<a href="baojian/qyts/">情欲提升</a>
+			<a href="baojian/">更多></a>
+		</div>
+
+
+		<div class="bgw mt10">
+			<h2 class="t">
+				<a href="neiyi/" class="brl">情趣内衣</a>
+				<a href="neiyi/" class="more right"></a>
+			</h2>
+			<div class="hpx ov alC" style="background-color:#e85f6d">
+				<a href="goods-4976.html?mps=index" class="left">
+					<img src="http://s.qw.cc/data/afficheimg/1440469760919184740.jpg" width="100%"/>
+				</a>
+				<a href="goods-5547.html?mps=index" class="left">
+					<img src="http://s.qw.cc/data/afficheimg/1454401429268931743.jpg" width="100%"/>
+				</a>
+				<a href="goods-5336.html?mps=index" class="left">
+					<img src="http://s.qw.cc/data/afficheimg/1457427183871081557.jpg" width="100%"/>
+				</a>
+			</div>
+		</div>
+		<div class="m_z ov">
+			<a href="neiyi/huola/" class="red fB">三点激情</a>
+			<a href="neiyi/qunzhuang/">性感裙装</a>
+			<a href="neiyi/dingziku/">情趣内裤</a>
+			<a href="neiyi/zhifu/" class="red fB">制服诱惑</a>
+			<a href="neiyi/wangyi/">连体网衣</a>
+			<a href="neiyi/siwa/" class="red fB">情趣丝袜</a>
+			<a href="neiyi/nsny/">男士内衣</a>
+			<a href="neiyi/">更多></a>
+		</div>
+
+		<div class="bgw mt10">
+			<h2 class="t">
+				<a href="biyuntao/" class="brl">避孕套</a>
+				<a href="biyuntao/" class="more right"></a>
+			</h2>
+			<div class="hpre ov" style="background-color:#5e5f6a">
+				<a href="goods-5426.html?mps=index" class="w36 left">
+					<img src="http://s.qw.cc/data/afficheimg/1454400051278030835.jpg" width="100%"/>
+				</a>
+				<a href="goods-390.html?mps=index" class="w64 left">
+					<img src="http://s.qw.cc/data/afficheimg/1452225037228907884.jpg" width="100%"/>
+				</a>
+			</div>
+		</div>
+		<div class="m_z ov">
+			<a href="biyuntao/chaobao/" class="red fB">超薄体贴</a>
+			<a href="biyuntao/luowen/">螺纹刺激</a>
+			<a href="biyuntao/chijiu/" class="red fB">超凡持久</a>
+			<a href="biyuntao/fudian/">浮点颗粒</a>
+			<a href="biyuntao/guowei/">果味热情</a>
+			<a href="biyuntao/chaobao/b26.html">冈本</a>
+			<a href="biyuntao/chaobao/b39.html">杜蕾斯</a> 
+			<a href="biyuntao/">更多></a>
+		</div>
+
+		<div class="hmg" style="background-color:#0c0d0f;">
+			<a href="topic.php?topic_id=1276">
+				<img src="http://s.qw.cc/data/afficheimg/1459248488096346458.jpg">
+			</a>
+		</div>
+		
+		<div class="bgw">
+			<h2 class="t"><a href="hot.html" class="brl">精品推荐</a><a href="hot.html" class="more right"></a></h2>
+			<div class="gd bigm">
+				<a href="goods-5415.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201604/goods_img/5415_P_1460455530149.jpg" class="gm"><h3 class="gt">藏帝 高原植物延时喷剂 不麻木无依赖 15ml</h3><p class="lp"><em class="xj">¥198.00</em><em class="s_s">销量:18880</em></p></div></a>
+				<a href="goods-5503.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201602/goods_img/5503_P_1456297109261.jpg" class="gm"><h3 class="gt">琦莎HAPPY两用口阴互换舌震免提飞机杯</h3><p class="lp"><em class="xj">¥199.00</em><em class="s_s">销量:14456</em></p></div></a>
+				<a href="goods-6017.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201602/goods_img/6017_P_1456367705335.jpg" class="gm"><h3 class="gt">香港简爱月亮之上自动抽插多频快感智能加温震动棒</h3><p class="lp"><em class="xj">¥368.00</em><em class="s_s">销量:9292</em></p></div></a>
+				<a href="goods-5520.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201511/goods_img/5520_P_1446535221453.jpg" class="gm"><h3 class="gt">奇乐圆Q-LUV-U无线遥控自动抽插电动仿真阳具</h3><p class="lp"><em class="xj">¥329.00</em><em class="s_s">销量:8425</em></p></div></a>
+				<a href="goods-6002.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201511/goods_img/6002_P_1446719027726.jpg" class="gm"><h3 class="gt">香港简爱定海神针智能恒温高潮冲击棒</h3><p class="lp"><em class="xj">¥338.00</em><em class="s_s">销量:4254</em></p></div></a>
+				<a href="goods-4776.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201510/goods_img/4776_P_1445220267589.jpg" class="gm"><h3 class="gt">漫缈ManMiao国优名器兽兽宝贝私处仿真倒模</h3><p class="lp"><em class="xj">¥199.00</em><em class="s_s">销量:4018</em></p></div></a>
+				<a href="goods-5321.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201602/goods_img/5321_P_1456130168598.jpg" class="gm"><h3 class="gt">香港诺兰自动伸缩抽插硅胶震动转珠棒</h3><p class="lp"><em class="xj">¥999.00</em><em class="s_s">销量:3823</em></p></div></a>
+				<a href="goods-5315.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201512/goods_img/5315_P_1450668595407.jpg" class="gm"><h3 class="gt">香港VNA清纯女神莎拉宝贝臀部倒模</h3><p class="lp"><em class="xj">¥299.00</em><em class="s_s">销量:3649</em></p></div></a>
+				<a href="goods-6007.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201504/goods_img/6007_P_1429775691608.jpg" class="gm"><h3 class="gt">UZ诱芷 催欲高潮提升精油  10ml</h3><p class="lp"><em class="xj">¥268.00</em><em class="s_s">销量:2462</em></p></div></a>
+				<a href="goods-5702.html" class="ga"><div class="pm"><img src="http://s.qw.cc/t/300x300/images/201603/goods_img/5702_P_1459148850504.jpg" class="gm"><h3 class="gt">JJ增大长效治疗组合 送水晶套</h3><p class="lp"><em class="xj">¥497.00</em><em class="s_s">销量:1269</em></p></div></a>
+			</div>
+		</div>
+		
+		<div class="bgw ov mt10">
+			<h2 class="t"><a href="fenlei.html" class="brl">商品分类</a><a href="fenlei.html" class="more right"></a></h2>
+			<div class="ov" id="fcat">
+				<a href="man/" class="b_ca"><h3 class="c3">男性用品</h3><p class="c9">飞机杯\真人倒模</p><img src="http://s.qw.cc/mobile/ui/h1.jpg" class="w_ct"/></a>
+				<a href="woman/" class="b_ca"><h3 class="c3">女性用品</h3><p class="c9">跳蛋\男根\转珠棒</p><img src="http://s.qw.cc/mobile/ui/h2.jpg" class="w_ct"/></a>
+				<a href="neiyi/" class="b_xa"><h3 class="c3">情趣内衣</h3><p class="c9">三点/制服/丝袜</p><img src="http://s.qw.cc/mobile/ui/h4.jpg" class="w_cb"/></a>
+				<a href="xingai/" class="b_xa"><h3 class="c3">双人情趣</h3><p class="c9">夫妻玩具\SM</p><img src="http://s.qw.cc/mobile/ui/h5.jpg" class="w_cb"/></a>
+				<a href="runhuaye/" class="b_xa"><h3 class="c3">润滑液</h3><p class="c9">让爱水多多</p><img src="http://s.qw.cc/mobile/ui/h6.jpg" class="w_cb"/></a>
+				<a href="baojian/" class="b_xa"><h3 class="c3">延时助情</h3><p class="c9">延长<i class="h">性爱时间</i></p><img src="http://s.qw.cc/mobile/ui/h3.jpg" class="w_cb"/></a>
+				<a href="biyuntao/" class="b_xa"><h3 class="c3">避孕套</h3><p class="c9">超薄\螺纹刺激</p><img src="http://s.qw.cc/mobile/ui/h7.jpg" class="w_cb"/></a>
+				<a href="fengqing/" class="b_xa"><h3 class="c3">丰胸缩阴</h3><p class="c9">恢复少女紧致</p><img src="http://s.qw.cc/mobile/ui/h8.jpg" class="w_cb"/></a>
+				<a href="woman/bizhen/" class="b_xa"><h3 class="c3">逼真男根</h3><p class="c9">超真实体验</p><img src="http://s.qw.cc/mobile/ui/h9.jpg" class="w_cb"/></a>
+				<a href="man/feijibei/" class="b_xa"><h3 class="c3">飞机杯</h3><p class="c9">女神陪你一起打</p><img src="http://s.qw.cc/mobile/ui/h10.jpg" class="w_cb"/></a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="fot">
+	<ul class="nav">
+		<li>
+			<a href="index.php" class="na">首页</a>
+			<a href="fenlei.html" class="na">分类</a>
+			<a href="car.php" class="na">购物车</a>
+			<a href="hot.html"  class="na">热销</a>
+		</li>
+		<li>
+			<a href="javascript:;" onClick="goback()" class="na">上一页</a>
+			<a href="login.php" class="na">登录/注册</a><a href="chaxun.php"  class="na">查订单</a><a href="javascript:;" onClick="gotop()" class="na">回顶部</a>
+		</li>
+		<li>
+			<a href="tel:4006600606" class="fbtn">400-660-0606<p class="f12">点击电话订购</p></a>
+			<a href="sms:13883873375" class="fbtn">点击短信订购<p class="f12">姓名+货号+数量+地址</p></a>
+		</li>
+	</ul>
+	<div class="lh25 mt10">
+		<a href="app.php" class="red">APP下载</a> | <a href="chat.php">在线客服</a> | <a href="article.php?act=detail&a_id=617">帮助</a> | <a href="faq.php">反馈</a> | <a href="about.php">关于我们</a>
+	</div>
+</div>
+
+<?php $this->load->view('layout/footer');?>
