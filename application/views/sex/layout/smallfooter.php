f608eaa9ddb11535aefbbe9dcf9de242afd406ed
604942578 <604942578@qq.com>
2016-12-22 19:30:29 +0800

    刚刚

--- application/views/sex/layout/smallfooter.php
+++ application/views/sex/layout/smallfooter.php
@@ -1,15 +1,15 @@
 <div class="fot">
 	<ul class="nav">
 		<li>
-			<a href="<?php echo site_url('qingqu/home/index');?>" class="na">首页</a>
-			<a href="<?php echo site_url('qingqu/home/category');?>" class="na">分类</a>
-			<a href="<?php echo site_url('qingqu/home/car')?>" class="na">购物车</a>
-			<a href="<?php echo site_url('qingqu/home/hot');?>"  class="na">热销</a>
+			<a href="<?php echo site_url('sex/home/index');?>" class="na">首页</a>
+			<a href="<?php echo site_url('sex/home/category');?>" class="na">分类</a>
+			<a href="<?php echo site_url('sex/home/car')?>" class="na">购物车</a>
+			<a href="<?php echo site_url('sex/home/hot');?>"  class="na">热销</a>
 		</li>
 		<li>
-			<a href="<?php echo site_url('qingqu/home/ucenter');?>"  class="na">个人中心</a>
-			<a href="<?php echo site_url('qingqu/home/login')?>" class="na">登录/注册</a>
-			<a href="<?php echo site_url('qingqu/home/query');?>"  class="na">查订单</a>
+			<a href="<?php echo site_url('sex/home/ucenter');?>"  class="na">个人中心</a>
+			<a href="<?php echo site_url('sex/home/login')?>" class="na">登录/注册</a>
+			<a href="<?php echo site_url('sex/home/query');?>"  class="na">查订单</a>
 			<a href="javascript:gotop();" class="na">回顶部</a>
 		</li>
 		<li>
@@ -18,8 +18,8 @@
 		</li>
 	</ul>
 	<div class="lh25 mt10">
-		<a href="<?php echo site_url('qingqu/home/help')?>">帮助</a> |
-		<a href="<?php echo site_url('qingqu/home/message');?>">反馈</a> |
-		<a href="<?php echo site_url('qingqu/home/about');?>">关于我们</a>
+		<a href="<?php echo site_url('sex/home/help')?>">帮助</a> |
+		<a href="<?php echo site_url('sex/home/message');?>">反馈</a> |
+		<a href="<?php echo site_url('sex/home/about');?>">关于我们</a>
 	</div>
 </div>
